fc935469fde70eb6c43d5b44ba4e58cd5caa0bec
Aravinth Panch <me@aravinth.info>
2020-09-29 23:46:08 +0200

    Update cv embed link of talent #1

--- 1/index.php
+++ 1/index.php
@@ -1,2 +1,2 @@
 <?php include('../cookie-consent.php');  ?>
-<embed src="madura-perera-cv.pdf" type="application/pdf" height="100%" width="100%" class="responsive">
+<embed src="cv.pdf" type="application/pdf" height="100%" width="100%" class="responsive">
