f272056a65e288d8aa5e3bc02102ffac333ad672
Aravinth Panch <me@aravinth.info>
2020-10-01 01:04:12 +0200

    Add talent #3

---
+++ 3/LI/index.php
@@ -0,0 +1,1 @@
+<?php header('Location: https://www.linkedin.com/in/neel-s-shah/'); exit(); ?>
---
+++ 3/STORY/index.php
@@ -0,0 +1,1 @@
+<embed src="../story.pdf" type="application/pdf" height="100%" width="100%" class="responsive">
